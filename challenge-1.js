6686ac566dd145e807502e1ab8f2cbe7485d44b1
AzizRahmawan <arder058@gmail.com>
2024-01-26 11:12:04 +0700

    challenge-1 first method

---
+++ challenge-1.js
@@ -0,0 +1,12 @@
+// First Method
+function countBy(x, n) {
+    let z = [];
+    let result = 0;
+    for(let i = 1; i <= n; i++){
+      result += x;
+      z.push(result);
+    }
+    return z;
+}
+
+console.log(countBy(2,5));
